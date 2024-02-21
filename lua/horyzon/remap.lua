79ba5ebd8a090b1e0a0ccff846ba21143e556a8a
kalyanoliveira <kalyan.c.o.010@gmail.com>
2024-02-21 12:56:50 -0500

    Updates mappings.

--- lua/horyzon/remap.lua
+++ lua/horyzon/remap.lua
@@ -4,12 +4,20 @@ vim.g.mapleader = " "
 -- Opening up Netrw (thanks Prime).
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- -- I'll learn these at some point:
--- vim.keymap.set("n", "J", "mzJ`z")
--- vim.keymap.set("n", "<C-d>", "<C-d>zz")
--- vim.keymap.set("n", "<C-b>", "<C-b>zz")
--- vim.keymap.set("n", "n", "nzzzv")
--- vim.keymap.set("n", "N", "Nzzzv")
+-- -- For moving selections around blazzingly fast.
+-- -- I won't be using these now, since I kind of use J in visual mode a lot.
+-- vim.keymap.set("v", "C-J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "C-K", ":m '<-2<CR>gv=gv")
+
+-- This makes your cursor stay where it was when you join two lines.
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- These just make jumping around buffers more convinient, as it'll also center
+-- your cursor in the middle of the screen.
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 -- Paste without absolutely destroying what you yanked in the past.
 -- This is useful when you want to highlight something in visual mode and paste
@@ -23,3 +31,7 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 -- Copy into the system clipboard.
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- We hate Q around here.
+vim.keymap.set("n", "Q", "<nop>")
