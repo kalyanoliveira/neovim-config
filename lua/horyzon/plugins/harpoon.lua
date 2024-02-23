18f719484b7847840c7049a0c66ad61377952399
kalyanoliveira <kalyan.c.o.010@gmail.com>
2024-02-22 19:58:44 -0500

    Switches harpoon keybinds to CTRL instead of <leader>

--- lua/horyzon/plugins/harpoon.lua
+++ lua/horyzon/plugins/harpoon.lua
@@ -5,10 +5,10 @@ return {
         local mark = require("harpoon.mark")
         local ui = require("harpoon.ui")
 
-        vim.keymap.set("n", "<leader>m", mark.add_file)
-
+        -- I did once intend on using <leader> instead of <C -, but the
+        -- Obsidian markdown text editor does not have support for that.
+        vim.keymap.set("n", "<C-m>", mark.add_file)
         vim.keymap.set("n", "<C-h>", ui.toggle_quick_menu)
-
         vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
         vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
         vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
